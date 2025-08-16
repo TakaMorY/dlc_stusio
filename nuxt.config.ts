93fe51b9895fc674c7a62bb4ca62e96eb3962d59
daniil <mail@gmail>
2025-08-16 17:00:40 +1000

    finish5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,11 +16,11 @@ export default defineNuxtConfig({
         { name: 'keywords', content: 'dlc,DLC,dlc надежденск ,Надежденкс,DLC Надежденск, Английский Надежденск, Английский, дошколка, разивашка Надежденск, развитие детей, посёлок Надежденкс, Надежденский, образование, dlc образование dlc studio' },
         { property: 'og:title', content: 'DLC — студия дополнительного образования' },
         { property: 'og:description', content: 'DLC — английский язык, школа раннего развития, поготовка к школе и продлёнка.' },
-        { property: 'og:image', content: 'https://dlc-stusio.ru/og-image.jpg' },
-        { property: 'og:url', content: 'https://dlc-stusio.ru' },
+        { property: 'og:image', content: 'https://dlc-studio.vercel.app/og/og-image.jpg' },
+        { property: 'og:url', content: 'https://dlc-studio.vercel.app/' },
       ],
       link: [
-        { rel: 'canonical', href: 'https://dlc-stusio.ru' },
+        { rel: 'canonical', href: 'https://dlc-studio.vercel.app/' },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, // Favicon
       ]
     }
